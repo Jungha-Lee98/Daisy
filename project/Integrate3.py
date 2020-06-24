28a2b0200c5d9e6ab5322ed6a6e21e9f611aa1e9
Jungha-Lee98 <leejungha199861@gmail.com>
2020-06-24 13:22:12 -0500

    integrate3

--- project/Integrate3.py
+++ project/Integrate3.py
@@ -1,63 +1,59 @@
-# this example requires PyAudio because it uses the Microphone class
-
+# import SR,pyttsx,sqlite3
 import speech_recognition as sr
 import pyttsx3
 import sqlite3
 
+# import pyttsx engine and set speech rate 
 engine = pyttsx3.init()
-rate = engine.getProperty('rate')   # getting details of current speaking rate
-print (rate)                        #printing current voice rate
-engine.setProperty('rate', 100) 
-
+rate = engine.getProperty('rate')               
+engine.setProperty('rate', 130)
+ 
+# start conversation 
 engine.say("Hi, I'm Daisy")
 engine.runAndWait()
-print("Hi, I'm Daisy") 
+print("Hi, I'm Daisy")
 
 # obtain audio from the microphone
 r = sr.Recognizer()
-# r.energy_threshold = 200
-# r.dynami  c_energy_threshold = True 
-with sr.Microphone(device_index=1, sample_rate=48000, chunk_size=1024) as source:
-    print("Please wait. Calibrating microphone...")
-    # r.energy_threshold = 5000   
-    # listen for 5 seconds and create the ambient noise energy level   
+
+# set Microphone instances
+with sr.Microphone(device_index=1, chunk_size=1024, sample_rate=48000) as source:
+    print("Please wait. Calibrating microphone...") 
     r.adjust_for_ambient_noise(source, duration=1) 
-    r.energy_threshold = 1000
+    r.energy_threshold = 500
     r.dynamic_energy_threshold = True
     r.dynamic_energy_adjustment_damping = 0.15
     r.dynamic_energy_adjustment_ratio = 1.5
-    r.pause_threshold = 0.8  
-    # r.dynamic_energy_threshold = True  
-   
+    r.pause_threshold = 0.8
+    
+    # speak to user 'say something!'
     engine.say("Say something!")
     engine.runAndWait()
     print("Say something!")
+
+    # get user's speech 
     audio = r.listen(source)
 
-# recognize speech using Sphinx
 try:
+    # recognize speech using Sphinx
     print("Sphinx thinks you said " + r.recognize_sphinx(audio))
     
-    all_words_list=[]
-    words_list = []
+    all_words_list=[] # all words list 
+    words_list = [] # words list except articles and be_verb 
 
+    # split sentence into words
     all_words_list = r.recognize_sphinx(audio).split(' ')
     print(all_words_list)
 
+    # remove articles an be_verbs
     for words in all_words_list:
-        if words!= 'a':
+        if (words == 'the') or (words =='is') or (words == 'a') or (words == 'of') or (words == 'an'):
+            continue
+        else:
             words_list.append(words)
     print(words_list)
 
-# word_list=['where', 'place','that','people' 'use','the', 'most']
-# word_list=['what','is','the','average','time','of','bicycles','for','members']
-# word_list=['what','average','age',"female",'users']
-# word_list=['how', 'many', 'customer','rent', 'bicycles']
-# word_list=['how', 'many', 'subscriber','rent', 'bicycles']
-# word_list=['how', 'many', 'females','rent', 'bicycles']
-
-    sqlquery = []
-
+    # when use avg function it run
     def avg():
         for i in range(len(words_list)):
             if words_list[i]=='average':
@@ -65,7 +61,11 @@ try:
                     return('avg(tripduration) ')
                 elif words_list[i+1] == 'age':
                     return('avg(tripduration) ')
+    
 
+    sqlquery = [] # sql_sentence's words list
+
+    # translate words into sql query
     for loop in words_list:
 
         if loop == 'what' or loop =='which' or loop =='where' or loop == "where's" or loop == 'how':
@@ -75,9 +75,9 @@ try:
             sqlquery.append("from_station_name ")
             sqlquery.append("from divvy_2015 group by from_station_name ")
 
-
         elif loop == 'average':
-            sqlquery.append(avg()) 
+            sqlquery.append(avg())
+            
 
         elif loop == 'each':
             sqlquery.append('from divvy_2015 ')
@@ -89,7 +89,6 @@ try:
         elif loop == 'most' or (loop == 'almost'):
             sqlquery.append("order by count(*) desc ")
             sqlquery.append("limit 1 ")
-            # sqlquery.insert(1,"count(*), ")
         
         elif loop == ('many'):
             sqlquery.append("count(*) ")
@@ -97,64 +96,64 @@ try:
         elif loop == ('customer'):
             sqlquery.append('from divvy_2015 where usertype="Customer" ')
                 
-        elif loop == ('subscriber') or loop== ('subscribers'):
+        elif loop == 'subscriber' or loop == 'subscribers':
             sqlquery.append('from divvy_2015 where usertype="Subscriber" ')
                 
         elif loop == 'female' or (loop == 'females'):
-            sqlquery.append('from divvy_2015 where gender="Female"')
-            
+            sqlquery.append('from divvy_2015 where gender="Female" ')
+        
+        
+    strsqlquery = ''.join(sqlquery) + ';'
+    print(strsqlquery)
 
-    print(''.join(sqlquery) + ';')
-
-
-
-
-    sqliteConnection = sqlite3.connect("C:\\Users\\이정하\\Documents\\Divvy_Trips_2015-Q1Q2\\Divvy.db")
+    # connect database,sqlite3
+    sqliteConnection = sqlite3.connect("C:\\Users\\USER\\divvy.db")
     cursor =sqliteConnection.cursor()
     print("Database created and Successfully Connected to SQLite")
-    sqlite_select_Query = "select sqlite_version();"
-    cursor.execute(sqlite_select_Query)
-    record = cursor.fetchall()
-    print("SQLite Database Version is: ", record)
 
-    sqlite_Q1 = ''.join(sqlquery) + ';'
-    cursor.execute(sqlite_Q1)
-    print("Success")
+    # execute sql query
+    cursor.execute(strsqlquery)
 
+    # cursor.fetchall(), print answer what user wants
     for row in cursor:
         print(row)
         print("\n")
 
-    answer_list=[]
+    answer_list=[] # words list to use answer
 
+    # remove question mark and print answer sentence
     for word in all_words_list:
         if (word == 'how') or (word =='many') or (word == 'what') or (word == 'where') or (word == 'which'):
             continue
         else:
             answer_list.append('{0} '.format(word))
     print(answer_list)
-
+    
+    # speak answer to user
     answer = ('{0}' +' '+ ''.join(answer_list)).format(row)
-
     print(answer)
-
     engine.say(answer)
     engine.runAndWait()
 
+    # close cursor
     cursor.close()
 
-    #engine.say(r.recognize_sphinx(audio))
-    #engine.runAndWait()
 except sr.UnknownValueError:
     print("Sphinx could not understand audio")
+    engine.say("Sphinx could not understand audio.Could you tell me question again?")
+    engine.runAndWait()
+
 except sr.RequestError as e:
     print("Sphinx error; {0}".format(e))
+    engine.say("I can't understand your question. Could you tell me question again?")
+    engine.runAndWait()
 
 except sqlite3.Error as error:
     print("Error while connecting to sqlite", error)
+    engine.say("I can't understand your question. Could you tell me question again?")
+    engine.runAndWait()
 
 finally:
     if (sqliteConnection):
         sqliteConnection.close()
         print("The SQLite connection is closed")
-
